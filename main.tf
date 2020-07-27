d91a32316d9f1743a486593bc88ba6deabb806ea
Kyle Ruddy <kmruddy@users.noreply.github.com>
2020-07-27 17:25:11 -0400

    Create main.tf
    Initial main.tf creation

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+variable env_type {
+  type    = set(string)
+  default = ["dev", "qa", "prod"]
+}
+
+module s3_bucket {
+  source   = "terraform-aws-modules/s3-bucket/aws"
+  for_each = var.env_type
+
+  bucket = "terraform-webinar-foreach-${each.key}"
+  region = var.aws_region
+}
